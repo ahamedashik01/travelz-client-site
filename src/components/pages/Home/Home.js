3e6b1c323582ca4ac1a2784925b509b05eb3e872
ahamedashik01 <ashikshamim.007@gmail.com>
2021-10-31 02:53:11 +0600

    home page added

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../../Banner/Banner';
+import Quality from '../../Quality/Quality';
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Quality></Quality>
+        </div>
+    );
+};
+
+export default Home;
