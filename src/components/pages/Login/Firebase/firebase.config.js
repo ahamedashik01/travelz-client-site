d78669580830bd1dd9de6d25818d3359df836f92
ahamedashik01 <ashikshamim.007@gmail.com>
2021-10-31 02:58:46 +0600

    firebase authentication and login page added

---
+++ src/components/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCleJ29z5JnzCsGR7IfKg6M9ZczTFAMnIo",
+    authDomain: "travelz-zz.firebaseapp.com",
+    projectId: "travelz-zz",
+    storageBucket: "travelz-zz.appspot.com",
+    messagingSenderId: "887992112402",
+    appId: "1:887992112402:web:7bc32fc0402d52cadf0fcb"
+};
+export default firebaseConfig;
